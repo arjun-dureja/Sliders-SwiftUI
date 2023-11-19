287baeeb57dd32adec8473b10d2dcc28e9109ad4
Arjun Dureja <arjun.dureja1000@gmail.com>
2023-11-18 23:20:00 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["Sliders"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/arjun-dureja/CGExtender.git", from: "1.0.1"),
+        .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.1"),
         .package(url: "https://github.com/arjun-dureja/SwiftUI-Shapes.git", from: "1.0.2"),
         .package(url: "https://github.com/kieranb662/bez.git", from: "1.0.0")
         // Dependencies declare other packages that this package depends on.
