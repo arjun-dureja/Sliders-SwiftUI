933811eb870b20aa2d85f09913b9cc4c7b599521
Arjun Dureja <arjun.dureja1000@gmail.com>
2023-11-18 23:16:37 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Sliders",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,8 +13,8 @@ let package = Package(
             targets: ["Sliders"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.1"),
-        .package(url: "https://github.com/kieranb662/Shapes.git", from: "1.0.2"),
+        .package(url: "https://github.com/arjun-dureja/CGExtender.git", from: "1.0.1"),
+        .package(url: "https://github.com/arjun-dureja/SwiftUI-Shapes.git", from: "1.0.2"),
         .package(url: "https://github.com/kieranb662/bez.git", from: "1.0.0")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
